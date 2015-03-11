db273be8219ab8432548369e97408255d5cf9621
Rohit Ghatol <rohit.ghatol@synerzip.com>
2015-03-11 10:24:56 -0700

    Added functionality to update, count, delete and exists. Added test cases for create, find, exists, delete. Added npm run coverage command to find the coverage. Pending test cases around more of mind scenarios and update scenarios

--- test/connector.test.js
+++ test/connector.test.js
@@ -7,17 +7,23 @@ var expect = require('chai').expect;
 describe('DataSource',function(){
   var ds = null;
   var User = null;
-  before(function(done){
+
+  beforeEach(function(done){
     ds = getDataSource();
-    User = ds.define('User1', {
+    User = ds.define('User', {
       id:{ type:String },
       name: { type: String},
       email: { type: String},
       age: Number
     });
+
     ds.connect(function(err,status){
       expect(err).to.be.null;
-      done();
+      User.destroyAll(function(err){
+        expect(err).to.be.null;
+        done();
+      });
+
     })
   });
 
@@ -31,16 +37,90 @@ describe('DataSource',function(){
     expect(ds.connector.settings.port).to.equal(6379);
   });
 
-  it('should be able to create Person',function(done){
+  it('should be able to create User',function(done){
 
-    User.create({id:'200',name: 'John'}, function (e, u) {
-
-      User.findById('200',function(err,user){
-        console.log(user);
+    var user1={id:'200',name: 'John'};
+    User.create(user1, function (err, user) {
+      expect(err).to.be.null;
+      expect(user.id).to.equal(user1.id);
+      expect(user.name).to.equal(user1.name);
+      User.findById('200',function(err,res){
+        expect(res.id).to.equal(user1.id);
+        expect(res.name).to.equal(user1.name);
         done();
       })
 
     });
+  });
+
+  it('should be able to destroy User',function(done){
+
+    var user1={id:'200',name: 'John'};
+    User.create(user1, function (err, user) {
+      expect(err).to.be.null;
+
+      User.findById('200',function(err,user){
+        expect(err).to.be.null;
+        expect(user.id).to.equal(user1.id);
+        expect(user.name).to.equal(user1.name);
+
+        User.deleteById('200',function(err){
+          expect(err).to.be.null;
+          done();
+        });
+      })
+
+    });
+  });
+
+  it('should be able to count User',function(done){
+
+    var user1={id:'200',name: 'John'};
+    var user2={id:'400',name: 'Bob'};
+    User.create(user1, function (err, user) {
+      expect(err).to.be.null;
+
+      User.count(function(err,count){
+        expect(count).to.equal(1);
+        User.create(user2, function (err, user) {
+          User.count(function(err,count){
+            expect(count).to.equal(2);
+            done();
+          })
+        });
+      });
+
+    });
+  });
+
+  it('should be able check if key exists',function(done){
+
+    var user1={id:'200',name: 'John'};
+    var user2={id:'400',name: 'Bob'};
+    User.create(user1, function (err, user) {
+      expect(err).to.be.null;
+      User.exists('200',function(err,res){
+        expect(res).to.equal(true);
+        User.create(user2, function (err, user) {
+          User.exists('400',function(err,res){
+            expect(res).to.equal(true);
+            done();
+          });
+        });
+
+      });
+
+    });
+  });
+
+  it('should not be able find a non existent User',function(done){
+
+    User.findById('200',function(err,user){
+      expect(user.id).to.be.undefined;
+      expect(user.name).to.be.undefined;
+      done();
+    })
+
   });
 
 });
